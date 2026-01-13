52896f30bb89895c9345f9eef909f46eaad90f1a
Mario Gstrein <mario.gstrein@bartenbach.com>
2026-01-13 13:29:36 +0100

    add debug-extension

---
+++ src/QuestPDF.Markdown/Extensions/PreviewExtensions.cs
@@ -0,0 +1,12 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.Markdown.Extensions
+{
+    internal static class PreviewExtensions
+    {
+        internal static IContainer Preview(this IContainer container, string text, bool show) =>
+            !string.IsNullOrEmpty(text) && show ? container.DebugArea(text, Colors.Blue.Darken1) : container;         
+    }
+}
